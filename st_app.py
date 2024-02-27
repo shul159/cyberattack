f8381cd9fc132c2609ea68fbc02417c0708d4122
shul159 <161179214+shul159@users.noreply.github.com>
2024-02-27 14:16:24 -0500

    Add files via upload

--- st_app.py
+++ st_app.py
@@ -9,7 +9,7 @@ from  helper import data_type
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-trained_model = dill.load(open('dt_model.dill', 'rb'))
+trained_model = dill.load(open(r'C:/Users/liush/Downloads/rf_model.dill', 'rb'))
 
 # @st.cache_data(experimental_allow_widgets=True) 
 def main():
@@ -38,23 +38,32 @@ def main():
 
     st.subheader('EDA')
     if st.button('Analyze data'): 
-        st.write('Correlations of selected numerical features')
-        selected_numerical_features = ['icmp.seq_le', 'icmp.checksum', 'http.content_length', 'tcp.connection.rst', 'tcp.ack', 'mqtt.topic_len']
-        correlation_matrix = X[selected_numerical_features].corr()
-        fig, ax = plt.subplots()
+        st.write('Correlations of numerical features')
+        # selected_numerical_features = ['icmp.seq_le', 'icmp.checksum', 'http.content_length', 'tcp.connection.rst', 'tcp.ack', 'mqtt.topic_len']
+        # correlation_matrix = X[selected_numerical_features].corr()
+        numerical_features = ['icmp.checksum', 'icmp.seq_le', 'tcp.ack', 'tcp.ack_raw', 
+                      'tcp.checksum', 'tcp.len', 'tcp.seq', 'udp.stream', 'dns.qry.name',
+                      'http.content_length',  'tcp.connection.fin', 'tcp.connection.rst', 'tcp.connection.syn',
+                      'tcp.connection.synack', 'tcp.flags', 'tcp.flags.ack',
+                      'udp.time_delta', 'dns.qry.qu', 'dns.retransmission', 'dns.retransmit_request',
+                      'mqtt.conflag.cleansess', 'mqtt.hdrflags', 'mqtt.len',  'mqtt.msgtype', 'mqtt.proto_len',
+                      'mqtt.topic_len', 'mqtt.ver', 'mqtt.conflags']
+        correlation_matrix = X[numerical_features].corr()
+        
+        fig, ax = plt.subplots(figsize=(15, 15))
         sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
         st.pyplot(fig)
         plt.clf()
         
-        st.write('Counts of selected categorical features')
-        selected_categorical_features = ['mqtt.protoname', 'http.response', 'http.request.method', 'mqtt.conack.flags']
-        fig, axes = plt.subplots(2, 2, figsize=(8, 8))
-        ax = axes.flat
-        fig.subplots_adjust(wspace=0.5)
-        for i, col in enumerate(selected_categorical_features):
-            sns.countplot(data=X[selected_categorical_features], x=col, ax=ax[i])
-        st.pyplot(fig)
-        plt.clf() 
+        st.write('Counts of categorical features')
+        # selected_categorical_features = ['mqtt.protoname', 'http.response', 'http.request.method', 'mqtt.conack.flags']
+        categorical_features = ['http.request.method', 'http.referer', 'http.response', 'mqtt.conack.flags', 'mqtt.protoname',
+                                'mqtt.topic', 'http.request.version', 'dns.qry.name.len']
+        for i, col in enumerate(categorical_features):
+            fig, ax = plt.subplots(1, 1)
+            X[col].value_counts().plot(kind='bar', ax=ax)
+            st.pyplot(fig)
+            plt.clf() 
   
     st.subheader('Prediction') 
     if st.button('Predict'):
@@ -76,6 +85,19 @@ def main():
             st.pyplot(fig2)
             plt.clf()
             
+            if attack_count == 0:
+                st.write('Great! Your network is safe!')
+            else:
+                st.write('Danger! Your network is under attack!')
+                st.write('Here is something you can do')
+                st.markdown(f"[{'Cyberattack'}]({'https://en.wikipedia.org/wiki/Cyberattackt'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Denial-of-service attack'}]({'https://en.wikipedia.org/wiki/Denial-of-service_attack'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Man-in-the-middle attack'}]({'https://en.wikipedia.org/wiki/Man-in-the-middle_attack'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Code injection'}]({'https://en.wikipedia.org/wiki/Code_injection'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Malware'}]({'https://en.wikipedia.org/wiki/Malware'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Ransomware'}]({'https://en.wikipedia.org/wiki/Ransomware'})", unsafe_allow_html=True)
+                st.markdown(f"[{'TCP/IP stack fingerprinting'}]({'https://en.wikipedia.org/wiki/TCP/IP_stack_fingerprinting'})", unsafe_allow_html=True)
+                st.markdown(f"[{'Port scanner'}]({'https://en.wikipedia.org/wiki/Port_scanner'})", unsafe_allow_html=True)
 
             
             # st.write(cr)
