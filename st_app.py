c049ce8e606ffbc491a7cc22132319b47cb7ed4a
shul159 <161179214+shul159@users.noreply.github.com>
2024-02-27 12:16:28 -0500

    Add files via upload

--- st_app.py
+++ st_app.py
@@ -5,9 +5,11 @@ import io
 import pandas as pd
 import dill
 import streamlit as st
-from  helper import plot_confusion_matrix,plot_confusion_matrix,plot_precision_recall_curve,compute_metrics, data_type
+from  helper import data_type
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-trained_model = dill.load(open('model.dill', 'rb'))
+trained_model = dill.load(open(r'C:/Users/liush/Downloads/rf_model.dill', 'rb'))
 
 # @st.cache_data(experimental_allow_widgets=True) 
 def main():
@@ -28,27 +30,55 @@ def main():
         try:
             ## read the uploaded csv file
             
-            df = pd.read_csv(file, low_memory=False, dtype=data_type)
-            X,y = df.drop(columns=['Attack_label','Attack_type']), df['Attack_label']
+            X = pd.read_csv(file, low_memory=False, dtype=data_type)
             st.subheader('Data')
-            st.write(df.head())
+            st.write(X.head())
         except Exception as e:
             st.write(str(e))
-            
+
+    st.subheader('EDA')
+    if st.button('Analyze data'): 
+        st.write('Correlations of selected numerical features')
+        selected_numerical_features = ['icmp.seq_le', 'icmp.checksum', 'http.content_length', 'tcp.connection.rst', 'tcp.ack', 'mqtt.topic_len']
+        correlation_matrix = X[selected_numerical_features].corr()
+        fig, ax = plt.subplots()
+        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+        st.pyplot(fig)
+        plt.clf()
+        
+        st.write('Counts of selected categorical features')
+        selected_categorical_features = ['mqtt.protoname', 'http.response', 'http.request.method', 'mqtt.conack.flags']
+        fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+        ax = axes.flat
+        fig.subplots_adjust(wspace=0.5)
+        for i, col in enumerate(selected_categorical_features):
+            sns.countplot(data=X[selected_categorical_features], x=col, ax=ax[i])
+        st.pyplot(fig)
+        plt.clf() 
+  
+    st.subheader('Prediction') 
     if st.button('Predict'):
         try:
             st.write('Start predicting')
             predictions = trained_model.predict(X)
-            st.write('Computing metricsy')
-            cm, cr = compute_metrics(  trained_model, X, y, trained_model.classes_)
-            ## plot the results
             
-            st.subheader('Confusion Matrix')
-            fig = plot_confusion_matrix(cm, ['Normal', 'Attack'])
+            st.write('Normal vs. Attack')
+            normal_count = (predictions == 'Normal').sum()
+            attack_count = len(predictions) - normal_count
+            ax = pd.DataFrame({'Normal': [normal_count], 'Attack': [attack_count]}).plot(kind='bar')
+            fig = ax.figure
             st.pyplot(fig)
+            plt.clf()
             
-            st.subheader('Classification Report')
-            st.write(cr)
+            st.write('Attack Types')
+            ax2 = pd.DataFrame({'Attack_type':predictions}).Attack_type.value_counts().sort_values(ascending=True).plot(kind='barh')
+            fig2 = ax2.figure
+            st.pyplot(fig2)
+            plt.clf()
+            
+
+            
+            # st.write(cr)
             
         except Exception as e:
             st.write(str(e))
